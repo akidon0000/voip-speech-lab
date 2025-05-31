6789ee4aec00ac933a195030d6a173e933df3edb
akidon0000 <akihiro.matsuyama@sansan.com>
2025-05-31 15:41:16 +0900

    Initial Commit

---
+++ voip-speech-lab/voip_speech_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  voip_speech_labApp.swift
+//  voip-speech-lab
+//
+//  Created by Akihiro Matsuyama on 2025/05/31.
+//
+
+import SwiftUI
+
+@main
+struct voip_speech_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
